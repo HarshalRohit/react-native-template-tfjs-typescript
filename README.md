22a7484fe05d714c0f18574411050097ddb730fe
Harshal Rohit <rohitharshal95@gmail.com>
2021-03-18 15:57:57 +0530

    first version

---
+++ README.md
@@ -0,0 +1,14 @@
+# React Native Template Tensorflowjs Typescript
+> A React Native template pre-configured with tensorflowjs and Typescript
+
+## Features
+TODO:
+
+## Usage
+```sh
+npx react-native init MyApp --template 
+```
+
+
+
+
---
+++ template.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    // Placeholder used to rename and replace in files
+    // package.json, index.json, android/, ios/
+    placeholderName: "RnTfjsTemplate",
+  
+    // Directory with template
+    templateDir: "./RnTfjsTemplate",
+  
+    // Path to script, which will be executed after init
+    // postInitScript: "./script.js"
+  };
+  
